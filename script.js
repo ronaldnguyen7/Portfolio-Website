cd2e40c56471a01f95d942989553ead3f5aaff6c
ronaldnguyen7 <ronaldnguyen555@gmail.com>
2024-12-26 18:00:37 -0500

    template done

--- script.js
+++ script.js
@@ -37,15 +37,62 @@ const projects = [
 
 const projectRow = document.getElementById("project-row")
 
-const projectBox = ( {id, name, skills, description, link, photo} ) => {
-    return `<div id="${id}" class="col-12 col-md-6 col-xl-4 p-2 project-container"> 
-        <img class="project-photo" src="${photo}">
 
-        <h3 id="project-title-text">${name}</h3>
+const skillButton = (skill) => {
+    return `<button class="skill-button">
+        ${skill}
+    </button>`
+}
+
+const projectBox = ( {id, name, skills, description, link, photo} ) => {
+    return `<div id="${id}" class="col-12 col-md-6 col-lg-4 p-2 project-container">
+        <div class="project-wrapper p-2"> 
+            <img class="project-photo" src="${photo}">
+
+            <h3 id="project-title-text">${name}</h3>
         <h4 id="project-description-text">${description}</h4>
-        <h4 id="skills">${ skills.map(skill => `#${skill}`).join(' ') }</h4>
+        
+        
+        ${ skills.map( skill => skillButton(skill)).join('')  }
+        
+
         <button id="github-link"><a href="${link}">GitHub</a></button>
+        </div>
     </div>`
 }
 
 projectRow.innerHTML += projects.map( (project) => projectBox(project) ).join('')
+
+const jobs = [
+    {
+        id : 0,
+        company : "Imperial Lion Dance",
+        title : "Web Development Intern",
+        description : "Redesign and redevelop organization's website using",
+        skills : ["JavaScript"],
+        beginning : "May 2024",
+        ending : "August 2024"
+    }
+]
+
+const jobRow = document.getElementById("experience-row")
+
+const jobBox = ( job ) => {
+    return `<div class="job-container d-flex align-items-center">
+        <div class="job-wrapper">
+            <div class=" row job-title-container d-flex align-items-center justify-content-between">
+                <p id="job-title-text" class="col-12 col-xl-4">${job.title}</p>
+
+                <p id="job-length-text" class="col-12 col-xl-4">${job.beginning} - ${job.ending}</p>
+            </div>
+
+            <p id="job-company-text">${job.company}</p>
+
+            <p id="job-description-text">${job.description}</p>
+
+            ${ job.skills.map( skill => skillButton(skill)).join('')  }
+        </div>
+    </div>`
+}
+
+jobRow.innerHTML += jobs.map ( (job) => jobBox(job) ).join("")
